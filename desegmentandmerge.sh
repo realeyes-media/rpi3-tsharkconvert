8f1597d7f7a04b3689f6ca7e3e293ed385263401
marcymarcy <marcy@realeyes.com>
2018-05-29 16:07:14 -0600

    Adding bash script

--- desegmentandmerge.sh
+++ desegmentandmerge.sh
@@ -1,14 +1,34 @@
 #!/bin/bash
-set -x
+set +x
+set -e
+
+# Set output file default or overwrite with CLI input
+OUTPUTNAME="outputParsedAndCombined.pcap"
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied - setting default output to outputParsedAndCombined.pcap"
+    OUTPUTNAME="outputParsedAndCombined.pcap"
+elif [ $# -eq 1 ]
+  then
+    echo "Setting output file to $1"
+    OUTPUTNAME="$1"
+else
+  echo "That's too many arguments; I don't play that stuff."
+  exit 1
+fi
 
 # Gather a list of pcap files in this directory
 declare -a FILESTODO=()
 CURRENT=$(pwd)
-for file in *.pcap; do
-  [ -f "$file" ] || break
+THESEFILES=$(ls *.pcap)
+for file in $THESEFILES; do
+  echo "Adding $file"
   THISFILE="$CURRENT$file"
-  FILESTODO+="$THISFILE"
+  FILESTODO+=("$THISFILE")
 done
 
-# Echo out files about to be combined
-echo ${myarray[@]}
+# Combine files
+mergecap -v ${FILESTODO[@]} -w tempoutputcombined.pcap
+
+# Use TShark to desegment TCP packets
+tshark -r tempoutputcombined.pcap -R "http" -o tcp.desegment_tcp_streams:TRUE -w $OUTPUTNAME
